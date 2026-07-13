01623b643e5031cea45a588c5146eb969357b59b
sarthakGup05 <rythmgupta@outlook.com>
2026-07-13 21:05:20 +0530

    feat: structure Expo Router layout groups and setup 404 handler

--- apps/mobile/app/index.tsx
+++ apps/mobile/app/index.tsx
@@ -2,14 +2,8 @@ import { View, Text } from "react-native";
 
 export default function Home() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Portl 🚀</Text>
+    <View className="flex-1 justify-center items-center bg-zinc-950">
+      <Text className="text-blue-300 text-3xl font-bold">Ambit 🚀</Text>
     </View>
   );
 }
