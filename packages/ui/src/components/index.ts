049333c6e32617b8fed020ace60b7ca8665f3b0f
sarthakGup05 <rythmgupta@outlook.com>
2026-07-14 21:30:31 +0530

    feat(ui): add entry points and exports for shared components

---
+++ packages/ui/src/components/index.ts
@@ -0,0 +1,11 @@
+/// <reference path="../nativewind-env.d.ts" />
+
+export * from './Avatar';
+export * from './Badge';
+export * from './Button';
+export * from './Card';
+export * from './Input';
+export * from './Skeleton';
+export * from './Screen';
+export * from './Text';
+export * from './SectionHeader';
---
+++ packages/ui/src/index.ts
@@ -0,0 +1,6 @@
+/// <reference path="./nativewind-env.d.ts" />
+
+export * from './theme';
+export * from './components';
+export * from './utils/cn';
+
