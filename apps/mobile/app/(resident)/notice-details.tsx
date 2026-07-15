283a43ce58e0ba9935c76e1be6af218c003674ef
sarthakGup05 <rythmgupta@outlook.com>
2026-07-15 17:24:52 +0530

    feat(mobile): add notice details screen route

---
+++ apps/mobile/app/(resident)/notice-details.tsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import { NoticeDetailsScreen } from '../../src/features/notices/components/NoticeDetailsScreen';
+
+export default function NoticeDetailsRoute() {
+  return <NoticeDetailsScreen />;
+}
